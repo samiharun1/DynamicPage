22a3df14bbc299305e78bbb8c41df6f79d18fd6d
Sami Harun <sami.harun@hotmail.com>
2025-02-25 09:52:17 +0100

    fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Projects from './pages/Projects';
